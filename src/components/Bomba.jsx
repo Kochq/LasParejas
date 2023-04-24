49eb5af0457f2f598743dbe973d79404771fce98
kochq <matukoch@live.com>
2023-04-24 08:19:49 -0300

    Adding title card

--- src/components/Bomba.jsx
+++ src/components/Bomba.jsx
@@ -36,36 +36,39 @@ const Bomba = ({eq, encendido}) => {
   };
 
   return (
-    <div className='bomba'>
-      <img
-        className='bomba--img'
-        src={encendido == 0 ? './img/bomba.png' : './img/bomba.gif'}
-        alt='bomba hidraulica'
-      />
+    <div className='bomba' >
+      <h3 className='bomba--title'>Bomba 1</h3>
+      <div className='bomba--body'>
+        <img
+          className='bomba--img'
+          src={encendido == 0 ? './img/bomba.png' : './img/bomba.gif'}
+          alt='bomba hidraulica'
+        />
 
-      <Boton
-        href='https://relevar.com.ar/app/geopos.php?centro=-32.6686,-61.51465&punta=-32.6686,-61.51465&nombre=Cisterna de Agua&archivo_movil=Movil001.txt&carpeta=LASPAREJAS'
-        svg='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
-        svg2='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
-        BcColor='#009ae6'
-        NewBcColor='#0056b3'
-        width={23}
-      />
+        <Boton
+          href='https://relevar.com.ar/app/geopos.php?centro=-32.6686,-61.51465&punta=-32.6686,-61.51465&nombre=Cisterna de Agua&archivo_movil=Movil001.txt&carpeta=LASPAREJAS'
+          svg='M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z'
+          svg2='M15 10.5a3 3 0 11-6 0 3 3 0 016 0z'
+          BcColor='#009ae6'
+          NewBcColor='#0056b3'
+          width={23}
+        />
 
-      <button className='bomba--btn' onClick={()=> handleClick(eq)}>
-        {loading 
-          ? <img className='bomba--gif' src="./img/loading.gif" alt=""/>
-          : <svg
-            className='svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth='3'
-            stroke='currentColor'
-          >
-            <path d='M5.636 5.636a9 9 0 1012.728 0M12 3v9' />
-          </svg>
-        }
-      </button>
+        <button className='bomba--btn' onClick={()=> handleClick(eq)}>
+          {loading 
+            ? <img className='bomba--gif' src="./img/loading.gif" alt=""/>
+            : <svg
+              className='svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='3'
+              stroke='currentColor'
+            >
+              <path d='M5.636 5.636a9 9 0 1012.728 0M12 3v9' />
+            </svg>
+          }
+        </button>
+      </div>
     </div>
   );
 };
